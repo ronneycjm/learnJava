7245c64bb57a5a98981a2a57ac470be274484321
ronneycjm <285642624@qq.com>
2020-08-13 22:16:55 +0800

    First commit

---
+++ src/com/learn/helloWorld.java
@@ -0,0 +1,10 @@
+package com.learn;
+
+/*
+ * Created by Ronnie Cui 2020.08.13
+ */
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
